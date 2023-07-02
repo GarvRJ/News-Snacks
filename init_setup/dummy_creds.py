6a6934fc62f59734de7715a8ab3b0210e89a514a
GarvRJ <garvjhangiani@gmail.com>
2023-07-02 20:34:20 +0530

    Dummy credentials updated

--- init_setup/dummy_creds.py
+++ init_setup/dummy_creds.py
@@ -2,10 +2,10 @@ import hashlib
 
 # Credentials
 passphrase = "XXXXXXXXXX"
-# OBTAIN NEW PASSPHRASE FOR YOUR ACCOUNT ON https://grjcodes.com/newssnacks
+# OBTAIN NEW PASSPHRASE FOR YOUR ACCOUNT BY CONTACTING DEVELOPER.
 md5_hash = hashlib.md5()
 md5_hash.update(passphrase.encode("utf-8"))
 key = md5_hash.hexdigest()
 
 # API links
-# OBTAIN CONFIGURATION FOR THIS SECTION AFTER MAKING ACCOUNT ON https://grjcodes.com/newssnacks
+# OBTAIN CONFIGURATION FOR THIS SECTION AFTER MAKING ACCOUNT BY CONTACTING DEVELOPER.
